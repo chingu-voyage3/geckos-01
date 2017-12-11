58dc815ef20a5e8ae4e3aa49cdf1b8ad1ff33a9b
Colin Nash <colinjnash@gmail.com>
2017-12-10 21:36:32 -0500

    Test branch. Still broken

--- expressTest/app.js
+++ expressTest/app.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var express = require("express");
 var app = express();
+var router = express.Router();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -12,45 +13,45 @@ app.use(bodyParser.urlencoded({extended: true}));
 var resources = ["YouTube Video", "Cool Book", "Nice Course"];
 
 /* ROUTES */
-app.get("/", function(req, res) {
+router.get("/", function(req, res) {
   res.redirect("/index");
 });
 
 // INDEX ROUTES
-app.get("/index", function(req, res) {
+router.get("/index", function(req, res) {
   res.render("index", {resources: resources});
 });
 
 // LOGIN ROUTES
-app.get("/login", function(req, res) {
+router.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.post("/login", function(req, res) {
+router.post("/login", function(req, res) {
   res.send("You logged in.");
 });
 
 // LOGOUT ROUTE
-app.get("/logout", function(req, res) {
+router.get("/logout", function(req, res) {
   res.send("You have logged out.");
 });
 
 // REGISTER ROUTE
-app.get("/register", function(req, res) {
+router.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res) {
+router.post("/register", function(req, res) {
   res.send("New user created.");
 });
 
 // RESOURCE ROUTE
-app.post("/resource", function(req, res) {
+router.post("/resource", function(req, res) {
   resources.push(req.body.resource);
   res.redirect("/index");
 });
 
 /* LISTEN */
-app.listen(3000, function() {
+app.listen(3001, function() {
   console.log("Server started...");
 });
