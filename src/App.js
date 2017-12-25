a1ff224119093ad8502e16fc3e7cfa57b40ab216
Karlyn Gibson <karlyn_gibson@hotmail.com>
2017-12-25 12:05:24 -0800

    add progress bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import HeadingNav from './components/heading-nav';
-import Milestones from './components/milestones';
+import ProgressBar from './components/progress-bar';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './App.css';
@@ -10,8 +10,18 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+
 				<HeadingNav />
-				<Milestones />
+
+				<h3>Javascript Guide</h3>
+
+				<div class="container">
+				<p id="progress-heading">Guide Progress:</p>
+					<ProgressBar /> 
+				</div>
+
+				
+				
 			</div>
 		);
 	}
